356dc40a6afec25c0f2c6de27378c41a295500cd
Zach Geckler <Zach.The.Gecko@gmail.com>
2024-08-12 18:06:29 -0600

    Created a function to parse data from IRIS.csv, a data set for identifying flowers

--- script.ts
+++ script.ts
@@ -1,30 +1,67 @@
+import csv from "csv-parser";
+import path from "path";
+import fs from "fs";
+
 interface Layer {
-  outputs: number;
-  layerPosition: number;
+  size: number;
+  position: number;
+}
+
+interface CSVRow {
+  [key: string]: string;
 }
 
 interface Network {
-  layerSizes: number[];
+  size: number[];
   layers: Layer[];
+  data: CSVRow[];
 }
 
+
+const parseCSV = (filePath: string): CSVRow[] => {
+  const fileContent = fs.readFileSync(path.resolve(filePath), "utf-8");
+  const lines = fileContent
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
+
+  const headers = lines[0].split(",");
+  const result: CSVRow[] = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const currentLine = lines[i].split(",");
+    const row: CSVRow = {};
+
+    headers.forEach((header, index) => {
+      row[header] = currentLine[index];
+    });
+
+    result.push(row);
+  }
+
+  return result;
+};
+
 class Layer {
-  constructor(inputs: number, outputs: number, layerPosition: number) {
-    this.inputs = inputs;
-    this.outputs = outputs;
-    this.layerPosition = layerPosition;
-    console.log("New layer created: ", sizeOfLayer, " and position: ", layerPosition);
+  constructor(size: number, position: number) {
+    this.size = size;
+    this.position = position;
   }
 }
 
 class Network {
-  constructor(layerSizes: number[]) {
-    this.layerSizes = layerSizes;
+  constructor(size: number[], data: CSVRow[]) {
+    this.size = size;
     this.layers = [];
-    for (let i: number = 0; i < layerSizes.length; i++) {
-      this.layers[i] = new Layer(layerSizes[i], i);
+    this.data = data;
+    for (let i = 0; i < size.length; i++) {
+      this.layers[i] = new Layer(size[i]);
     }
+  }
+  getNetworkOutput(inputs: number[]){
+    
   }
 }
 
-const myNetwork = new Network([1, 1, 1]);
+const myNetwork = new Network([2, 3, 3], parseCSV("./IRIS.csv"));
+myNetwork.calculateCost();
