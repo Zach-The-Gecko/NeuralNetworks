75326c1926289e8bf1d276c5975bb7d1245c0888
Zach Geckler <Zach.The.Gecko@gmail.com>
2024-08-14 10:19:17 -0600

    Added bias gradients, and added back the function to parse CSV. was finally able to 'train' a single input on a very simple network

--- funcScript.ts
+++ funcScript.ts
@@ -1,6 +1,12 @@
 // I could possibly remove the 'inputSize' attribute from the layerInfo object
+import fs from "fs";
+import path from "path";
 
 type activationFunction = (input: number) => number;
+
+interface CSVRow {
+  [key: string]: string;
+}
 
 interface NeuronInfo {
   bias: number;
@@ -103,7 +109,7 @@ const calculateCost = (outputs: number[], expectedOutputs: number[]) => {
   return cost;
 };
 
-const calculateGradientForSingleInput = (
+const calculateWeightGradientForSingleInput = (
   network: NetworkInfo,
   inputs: number[],
   expectedOutputs: number[]
@@ -132,31 +138,146 @@ const calculateGradientForSingleInput = (
   });
 };
 
-const updateNetwork = (network: NetworkInfo, weights: number[][][]) => {
-  const learnRate = 5;
-  weights.map((layerWeights, layerWeightsPosition) => {
-    layerWeights.map((neuronWeights, neuronWeightsPosition) => {
-      neuronWeights.map((weight, weightPosition) => {
-        network.layers[layerWeightsPosition].neurons[
-          neuronWeightsPosition
-        ].weights[weightPosition] -= weight * learnRate;
-      });
+const calculateBiasGradientForSingleInput = (
+  network: NetworkInfo,
+  inputs: number[],
+  expectedOutputs: number[]
+) => {
+  return network.layers.map((layer, layerPosition) => {
+    return layer.neurons.map((neuron, neuronPosition) => {
+      const previousNetworkOutputs = testInputsOnNetwork(inputs, network);
+      const previousNetworkCost = calculateCost(
+        previousNetworkOutputs,
+        expectedOutputs
+      );
+
+      network.layers[layerPosition].neurons[neuronPosition].bias += 0.00001;
+      const newNetworkOutputs = testInputsOnNetwork(inputs, network);
+      const newNetworkCost = calculateCost(newNetworkOutputs, expectedOutputs);
+
+      return (newNetworkCost - previousNetworkCost) / 0.00001;
     });
   });
-  return network;
 };
 
-// console.log(testInputsOnNetwork([1, 1], network));
+const updateNetwork = (
+  network: NetworkInfo,
+  weightGradients: number[][][],
+  biasGradients: number[][]
+) => {
+  const newNetwork: NetworkInfo = { layers: [], activationFunction: () => 0 };
+  newNetwork.layers = structuredClone(network.layers);
+  newNetwork.activationFunction = network.activationFunction;
+  newNetwork.layers = structuredClone(network.layers);
 
-// console.log(calculateCost([0.3, 0.2, 0.6], [0, 0, 1]));
+  const learnRate = 5;
+  weightGradients.map((layerWeightGradients, layerWeightGradientsPosition) => {
+    layerWeightGradients.map(
+      (neuronWeightGradients, neuronWeightGradientsPosition) => {
+        neuronWeightGradients.map((weightGradient, weightGradientPosition) => {
+          newNetwork.layers[layerWeightGradientsPosition].neurons[
+            neuronWeightGradientsPosition
+          ].weights[weightGradientPosition] -= weightGradient * learnRate;
+        });
+      }
+    );
+  });
 
-// deepLog(network);
-// console.log(testInputsOnNetwork([2, 3], network));
+  biasGradients.map((layerBiasGradients, layerBiasGradientsPosition) => {
+    layerBiasGradients.map((neuronBiasGradient, neuronBiasPosition) => {
+      newNetwork.layers[layerBiasGradientsPosition].neurons[
+        neuronBiasPosition
+      ].bias -= neuronBiasGradient * learnRate;
+    });
+  });
 
-const network = initializeNetwork([2, 3, 3, 2], sigmoid);
-const d1 = calculateGradientForSingleInput(network, [2, 2], [1, 0]);
+  return newNetwork;
+};
 
-const network2 = updateNetwork(network, d1);
-const d2 = calculateGradientForSingleInput(network2, [2, 2], [1, 0]);
+const trainOnSingleInputTEST = (
+  network: NetworkInfo,
+  depth: number,
+  inputs: number[],
+  expectedOutputs: number[]
+) => {
+  if (depth === 0) {
+    return network;
+  }
+  const weightGradients = calculateWeightGradientForSingleInput(
+    network,
+    inputs,
+    expectedOutputs
+  );
+  const biasGradients = calculateBiasGradientForSingleInput(
+    network,
+    inputs,
+    expectedOutputs
+  );
 
-// I'm close, but I also have to update gradient for biases, not just weights
+  const newNetwork = updateNetwork(network, weightGradients, biasGradients);
+  return trainOnSingleInputTEST(newNetwork, depth - 1, inputs, expectedOutputs);
+};
+
+const parseCSV = (filePath: string): CSVRow[] => {
+  const fileContent = fs.readFileSync(path.resolve(filePath), "utf-8");
+  const lines = fileContent
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
+
+  const headers = lines[0].split(",");
+  const result: CSVRow[] = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const currentLine = lines[i].split(",");
+    const row: CSVRow = {};
+
+    headers.forEach((header, index) => {
+      row[header] = currentLine[index];
+    });
+
+    result.push(row);
+  }
+
+  return result;
+};
+
+const mapSpeciesToExpectedOutput = (species: string) => {
+  switch (species) {
+    case "Iris-setosa":
+      return [1, 0, 0];
+    case "Iris-versicolor":
+      return [0, 1, 0];
+    case "Iris-virginica":
+      return [0, 0, 1];
+  }
+  return [0, 0, 0];
+};
+
+// WORK ON THIS ONE
+const trainNetworkOneIteration = (
+  network: NetworkInfo,
+  trainingData: number[][]
+) => {
+  const totalCost = trainingData.reduce((acc, [inputs, outputs]) => {
+    return acc;
+  }, 0);
+};
+
+const formattedData = parseCSV("IRIS.csv").map(
+  ({ sepal_length, sepal_width, petal_length, petal_width, species }) => {
+    return [
+      [
+        parseFloat(sepal_length),
+        parseFloat(sepal_width),
+        parseFloat(petal_length),
+        parseFloat(petal_width),
+      ],
+      mapSpeciesToExpectedOutput(species),
+    ];
+  }
+);
+
+console.log(formattedData);
+
+// const network = initializeNetwork([2, 2], sigmoid);
